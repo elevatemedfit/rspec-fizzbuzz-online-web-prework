cd72838612c419188377d1d144762453302526f5
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-14 20:37:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  end
   elsif int % 5 == 0
     "Buzz"
-  end
   elsif int % 3 == 0
    "Fizz"
-  end
-else 
+  else 
     "nil"
   end
 end
